52208d794ddae848016339f11d15a4dd8f04e1ee
Christelle <eloundou.c@gmail.com>
2017-07-09 17:24:05 +0200

    Pop up 1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector('html').onclick=function () {
-	if (document.querySelector('.content').style.display == 'none'){
+	if (document.querySelector('.content').style.display === 'none'){
 	document.querySelector('.content').style.display = 'block';
     } else {
     	document.querySelector('.content').style.display = 'block';
@@ -11,3 +11,7 @@ document.querySelector('.recherche').onclick=function () {
 	document.querySelector('.recherche').value = "";
     } 
 }
+
+setTimeout(function() {
+alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+},10000);
