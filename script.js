28a8b81524239cdc836d1e80af7c5a94c8a19602
Christelle <eloundou.c@gmail.com>
2017-07-09 15:42:13 +0200

    Faire disparaitre 'recherche' lorsqu'on clique dans la barre

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ document.querySelector('html').onclick=function () {
 	if (document.querySelector('.content').style.display == 'none'){
 	document.querySelector('.content').style.display = 'block';
     } else {
-    	document.querySelector('.content').style.display = 'none';
+    	document.querySelector('.content').style.display = 'block';
     }
+}
+
+document.querySelector('.recherche').onclick=function () {
+	if (document.querySelector('.recherche').value == "Rechercher..."){
+	document.querySelector('.recherche').value = "";
+    } 
 }
